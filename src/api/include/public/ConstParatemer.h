61b1aa9e17b06006dd4cf1a5c8779d91ce2f9e27
mfsjack <mfsjack@gmail.com>
2016-03-29 23:14:49 +0800

    creat project director

---
+++ src/api/include/public/ConstParatemer.h
@@ -0,0 +1,18 @@
+/******************************
+ *
+ *
+ *
+ *
+ *
+ * *************************/
+
+
+#ifndef CONST_VALUE_H
+#define CONST_VALUE_H
+
+const int OK = 0;
+const int ERROR = -1;
+
+
+
+#endif
